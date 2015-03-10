c50c2c162364d2cd2cf5df932a8f41184d964228
Maddie Fees <madison.fees90@gmail.com>
2015-03-10 08:36:06 -0600

    setting up some stuff for firebase, playing around using codeacademy and seeing what I can get to work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,13 @@
 /* jshint devel:true */
 console.log('\'Allo \'Allo!');
+
+var url = "https://madease.firebaseio.com";
+var firebaseRef = new Firebase(url);
+
+$(function() {
+    firebaseRef.set({
+        type: "fruit",
+        name: "apple",
+        price: 42.0
+    });
+});
