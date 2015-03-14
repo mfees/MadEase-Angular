9a003a5c2209f162f2548e580881ad97541734bd
Maddie Fees <madison.fees90@gmail.com>
2015-03-14 15:45:29 -0600

    fucking things up and fixing them, trying to figure out why facebook login not working, made some progress finally

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('madEase', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap'])
+angular.module('madEase', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'firebase'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
@@ -30,10 +30,106 @@ angular.module('madEase', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'u
         .state('login', {
         url: '/login',
         templateUrl: '../../states/login.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
     })
 
     $urlRouterProvider.otherwise('/');
+  })
+
+.factory('Auth', function($firebaseObject){
+    var auth = new Firebase('https://madease.firebaseio.com');
+    var currentUser = {};
+ 
+  return {
+    /**
+    * Wrapper for `onAuth` that filters the `auth` object
+    * through the `updateUser()` function
+    */
+    onAuth: function(creds){
+      auth.onAuth(function(data){
+        creds(updateUser(data));
+      });
+    },
+    /**
+    * Wrapper for `authWithOAuthPopup()` for each login option.
+    */
+        
+    fbLogin: function(){
+ 
+        return auth.authWithOAuthPopup("facebook", function(error, authData) {
+            console.log(authData)
+        if (error) {
+            console.log("Login Failed!", error);
+        } else {
+            $state.go("home");
+            console.log("Authenticated successfully with payload:", authData);
+        }
+      }, {remember: "sessionOnly"})
+    },
+        
+    /** Wrapper for the unauth() functionality to logout
+    */
+        logout: function(){
+        auth.unauth();
+        console.log("goodbye")
+    },
+    /** Wrapper to allow the main controller to check if a user is currently 
+    * Logged in currently
+    */
+    loggedIn: function(){
+      if(auth.getAuth()){
+        return true;
+      }
+    },
+    /**
+    *Get the current user.
+    */
+    getUser: function(){
+      return currentUser;
+    }
+  };
+ 
+  /**
+  * Tranform the `authdUser` object from `$firebaseAuth` into a full User
+  * record in the `/users` collection.
+  *
+  * @param {Object} authdUser from $firebaseAuth.getAuth()
+  * @return {Object} from $firebase.$asObject()
+  */
+  function updateUser(authdUser){
+      console.log(authdUser)
+    if ( authdUser === null ){
+      return null;
+    }
+    console.log("This will break if you login with anything other than FB")
+    /**
+    * Create a reference to the users collection within Firebase
+    * Then create a child of the users collection named after the
+    * authdUser's Facebook ID
+    */
+    var fbUser = auth.child('users').child(authdUser.facebook.id);
+
+    // Update the authdUser's information in Firebase
+    fbUser.update({
+      uid: authdUser.facebook.id,
+      facebook: authdUser.facebook,
+      fullName: authdUser.facebook.displayName,
+      firstName: authdUser.facebook.cachedUserProfile.first_name,
+      lastName: authdUser.facebook.cachedUserProfile.last_name,
+      avatarUrl: authdUser.facebook.cachedUserProfile.picture.data.url,
+    });
+
+    // Set user to the object reference of authdUser
+    fbUser = $firebaseObject(auth
+      .child('users')
+      .child(authdUser.facebook.id)
+    )
+
+    //stores the user information for use elsewhere
+    currentUser = fbUser;
+
+    return fbUser;
+   }
   })
 ;
