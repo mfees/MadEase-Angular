6cf83d28b309e4f99b13eda496b90399208240ba
Maddie Fees <madison.fees90@gmail.com>
2015-03-23 12:46:19 -0600

    moving files around for build, making the most recent function work on home page

--- src/app/controllers/add.closet.controller.js
+++ src/app/controllers/add.closet.controller.js
@@ -8,12 +8,12 @@ var app = angular.module('madEase')
         
     });  
     
-    document.getElementById("upload_widget_opener").addEventListener("click", function() {
-
-    cloudinary.openUploadWidget({ cloud_name: 'madease', upload_preset: 'ejxt3qdf'}, 
-    function(error, result) { console.log(error, result) });
-
-    }, false);  
+//    document.getElementById("upload_widget_opener").addEventListener("click", function() {
+//
+//    cloudinary.openUploadWidget({ cloud_name: 'madease', upload_preset: 'ejxt3qdf'}, 
+//    function(error, result) { console.log(error, result) });
+//
+//    }, false);  
       
     console.log(self.user);
     this.closet = new Firebase('https://madease.firebaseio.com/closets/' + self.user.$id);
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,10 +1,16 @@
 'use strict';
   angular.module('madEase')
-  .controller('MainCtrl', function (Auth) {
+  .controller('MainCtrl', function (Auth, $firebaseObject, $firebaseArray) {
         var self = this;
       
-    Auth.onAuth(function(user){
+        Auth.onAuth(function(user){
         self.user = user;
         console.log(user)
     });
+      
+        this.lastCloset = new Firebase('https://madease.firebaseio.com/closets/' + self.user.$id).limitToLast(1);
+        this.recent = $firebaseArray(this.lastCloset);
+      
+        console.log(this.recent)
+
   });
